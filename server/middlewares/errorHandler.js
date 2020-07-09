026b3a41abdb1ed7d0737718235cfd2104041059
luqmanabyadh <lqmn.abydh@gmail.com>
2020-07-09 19:03:26 +0700

    edit-error-handler-dan-rubah-jadi-switch-case

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,37 +1,54 @@
 module.exports = function (err, _, res, _) {
-
   let statusCode = 500
-  let errorCode = 'INTERNAL SERVER ERROR'
-  let message = 'Internal Server Error'
+  let message = "Internal Server Error"
 
-  if (err.name == 'SequelizeValidationError') {
-    statusCode = 400
-    errorCode = 'VALIDATION_ERROR'
-    const msg = []
-    for (let i = 0; i < err.errors.length; i++) {
-      msg.push(err.errors[i].message)
-    }
-    message = msg.join(', ')
-  } else if (err.name == 'EMAIL_ALREADY_EXIST') {
-    statusCode = 400
-    errorCode = 'EMAIL_ALREADY_EXIST'
-    message = 'Email Already Exist'
-  } else if (err.name == 'INVALID_EMAIL/PASSWORD') {
-    statusCode = 400
-    errorCode = 'INVALID_EMAIL/PASSWORD'
-    message = 'Invalid Email/Password'
-  } else if (err.name == 'ERROR_NOT_FOUND') {
-    statusCode = 404
-    errorCode = 'ERROR_NOT_FOUND'
-    message = 'Error Not Found'
-  } else if (err.name == 'PLEASE_LOGIN_FIRST') {
-    statusCode = 400
-    errorCode = 'PLEASE_LOGIN_FIRST'
-    message = 'Please Login First'
-  } else if (err.name == 'FORBIDDEN_ACCESS') {
-    statusCode = 403
-    errorCode = 'FORBIDDEN_ACCESS'
-    message = 'Forbidden Access'
+  switch (err.name) {
+    case "SequelizeValidationError":
+      message = []
+      err.errors.forEach(error => {
+        message.push(error.message)
+      })
+      statusCode = 400
+      break
+    case "SequelizeUniqueConstraintError":
+      message = []
+      err.errors.forEach(error => {
+        message.push(error.message)
+      })
+      statusCode = 400
+      break
+    case "ValidationError":
+      message = "Token not Found"
+      statusCode = 401
+      break
+    case "JsonWebTokenError":
+      message = "Token is invalid"
+      statusCode = 401
+      break
+    case "PLEASE_LOGIN_FIRST":
+      message = "Please Login First"
+      statusCode = 400
+      break
+    case "FORBIDDEN_ACCESS":
+      message = "Forbidden Access"
+      statusCode = 403
+      break
+    case "INVALID_EMAIL/PASSWORD":
+      message = "Invalid Email/Password"
+      statusCode = 400
+      break
+    case "EMAIL_ALREADY_EXIST":
+      message = "Email Already Exist"
+      statusCode = 400
+      break
+    case "ERROR_NOT_FOUND":
+      message = "Error not found"
+      statusCode = 404
+      break
+    case "COSTUM_ERROR":
+      message = error.message
+      statusCode = error.statusCode
+      break
   }
-  res.status(statusCode).json({ errorCode, message })
+  return res.status(statusCode).json({ message: message })
 }
